d19a1ebfa00c7fb91cf9359829d3430d3681f8ef
Talha Öden <133509406+TalhaODEN@users.noreply.github.com>
2024-11-22 11:16:38 +0300

    Staging commiting

--- Assets/Scripts/Menu/MenuInterface.cs
+++ Assets/Scripts/Menu/MenuInterface.cs
@@ -6,268 +6,196 @@ using UnityEngine.UI;
 
 public class MenuInterface : MonoBehaviour
 {
-<<<<<<< HEAD
+    #region Audio Settings
+    [Header("Audio")]
+    [SerializeField] private Slider audioSlider;
+    [SerializeField] private Button audioButton;
+    #endregion
+
+    #region Info Panel
+    [Header("Info")]
+    [SerializeField] private GameObject infoPanel;
+    private float timeToAutoClose = 5f;
+    private float currentTime = 0f;
+    #endregion
+
+    #region Music
+    [Header("Music")]
+    [SerializeField] private AudioSource menuMusic;
+    [SerializeField] private Button musicButton;
+    #endregion
+
+    #region Controls Panel
+    [Header("Controls Panel")]
+    [SerializeField] private GameObject controlsPanel;
+    [SerializeField] private Scrollbar controlScroll;
+    #endregion
+
+    #region FAQ
+    [Header("FAQ")]
+    [SerializeField] private GameObject FAQpanel;
+    [SerializeField] private Scrollbar faqScroll;
+    #endregion
+
+
+    #region Unity Methods
     private void OnEnable()
     {
         MenuEvents.OnPlayButtonClicked += StartGame;
         MenuEvents.OnControlsButtonClicked += ShowControls;
+        MenuEvents.OnCloseControlsPanelClicked += CloseControlsPanel;
         MenuEvents.OnMusicButtonClicked += ToggleMusic;
         MenuEvents.OnAudioButtonClicked += OpenAudioSettings;
         MenuEvents.OnQuestionButtonClicked += ShowFAQ;
+        MenuEvents.OnCloseFAQPanelClicked += CloseFAQ;
         MenuEvents.OnInfoButtonClicked += ShowInfo;
         MenuEvents.OnExitButtonClicked += ExitGame;
+
+        audioSlider.onValueChanged.AddListener(UpdateAudioSituation);
     }
 
     private void OnDisable()
     {
         MenuEvents.OnPlayButtonClicked -= StartGame;
         MenuEvents.OnControlsButtonClicked -= ShowControls;
+        MenuEvents.OnCloseControlsPanelClicked -= CloseControlsPanel;
         MenuEvents.OnMusicButtonClicked -= ToggleMusic;
         MenuEvents.OnAudioButtonClicked -= OpenAudioSettings;
         MenuEvents.OnQuestionButtonClicked -= ShowFAQ;
+        MenuEvents.OnCloseFAQPanelClicked -= CloseFAQ;
         MenuEvents.OnInfoButtonClicked -= ShowInfo;
         MenuEvents.OnExitButtonClicked -= ExitGame;
+
+        audioSlider.onValueChanged.RemoveListener(UpdateAudioSituation);
     }
-    public void OnPlayButton() => MenuEvents.PlayButtonClicked();
-    public void OnControlsButton() => MenuEvents.ControlsButtonClicked();
-    public void OnMusicButton() => MenuEvents.MusicButtonClicked();
-    public void OnAudioButton() => MenuEvents.AudioButtonClicked();
-    public void OnQuestionButton() => MenuEvents.QuestionButtonClicked();
-    public void OnInfoButton() => MenuEvents.InfoButtonClicked();
-    public void OnExitButton() => MenuEvents.ExitButtonClicked();
-    private void StartGame()
+    private void Update()
     {
-        SceneManager.LoadScene("MainGame");
-    }
-    private void ShowControls()
-    {
-        Debug.Log("Showing controls menu...");
-    }
-    private void ToggleMusic()
-    {
-        Debug.Log("Toggling music...");
-    }
-    private void OpenAudioSettings()
-    {
-        Debug.Log("Opening audio settings...");
-    }
-    private void ShowFAQ()
-    {
-        Debug.Log("Showing FAQ...");
-    }
-    private void ShowInfo()
-    {
-        Debug.Log("Showing game info...");
-    }
-    private void ExitGame()
-    {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false; 
-        #else
-            Application.Quit(); // Derlenmiş uygulamada oyunu kapatır
-        #endif
-    }
-
-
-
-
-
-
-
-
-
-=======
-    #region Audio Settings
-        [Header("Audio")]
-        [SerializeField] private Slider audioSlider;
-        [SerializeField] private Button audioButton;
-    #endregion
-
-    #region Info Panel
-        [Header("Info")]
-        [SerializeField] private GameObject infoPanel;
-        private float timeToAutoClose = 5f;
-        private float currentTime = 0f;
-    #endregion
-
-    #region Music
-        [Header("Music")]
-        [SerializeField] private AudioSource menuMusic; 
-        [SerializeField] private Button musicButton;
-    #endregion
-
-    #region Controls Panel
-        [Header("Controls Panel")]
-        [SerializeField] private GameObject controlsPanel;
-        [SerializeField] private Scrollbar controlScroll;
-    #endregion
-
-    #region FAQ
-        [Header("FAQ")] 
-        [SerializeField] private GameObject FAQpanel;
-        [SerializeField] private Scrollbar faqScroll;
-    #endregion
-
-
-    #region Unity Methods
-        private void OnEnable()
+        if (infoPanel.activeSelf && CheckInfoPanelTime())
         {
-            MenuEvents.OnPlayButtonClicked += StartGame;
-            MenuEvents.OnControlsButtonClicked += ShowControls;
-            MenuEvents.OnCloseControlsPanelClicked += CloseControlsPanel;
-            MenuEvents.OnMusicButtonClicked += ToggleMusic;
-            MenuEvents.OnAudioButtonClicked += OpenAudioSettings;
-            MenuEvents.OnQuestionButtonClicked += ShowFAQ;
-            MenuEvents.OnCloseFAQPanelClicked += CloseFAQ;
-            MenuEvents.OnInfoButtonClicked += ShowInfo;
-            MenuEvents.OnExitButtonClicked += ExitGame;
-
-            audioSlider.onValueChanged.AddListener(UpdateAudioSituation);
+            ShowInfo();
         }
-
-        private void OnDisable()
+        if (controlsPanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
         {
-            MenuEvents.OnPlayButtonClicked -= StartGame;
-            MenuEvents.OnControlsButtonClicked -= ShowControls;
-            MenuEvents.OnCloseControlsPanelClicked -= CloseControlsPanel;
-            MenuEvents.OnMusicButtonClicked -= ToggleMusic;
-            MenuEvents.OnAudioButtonClicked -= OpenAudioSettings;
-            MenuEvents.OnQuestionButtonClicked -= ShowFAQ;
-            MenuEvents.OnCloseFAQPanelClicked -= CloseFAQ;
-            MenuEvents.OnInfoButtonClicked -= ShowInfo;
-            MenuEvents.OnExitButtonClicked -= ExitGame;
-
-            audioSlider.onValueChanged.RemoveListener(UpdateAudioSituation);
+            CloseControlsPanel();
         }
-        private void Update()
+        if (FAQpanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
         {
-            if (infoPanel.activeSelf && CheckInfoPanelTime())
-            {
-                ShowInfo();
-            }
-            if (controlsPanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
-            {
-                CloseControlsPanel();
-            }
-            if (FAQpanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
-            {
-                CloseFAQ();
-            }
+            CloseFAQ();
         }
+    }
 
     #endregion
 
     #region Button Methods
-        public void OnPlayButton() => MenuEvents.PlayButtonClicked();
-        public void OnControlsButton() => MenuEvents.ControlsButtonClicked();
-        public void OnCloseControlsButton() => MenuEvents.CloseControlsPanelClicked();
-        public void OnMusicButton() => MenuEvents.MusicButtonClicked();
-        public void OnAudioButton() => MenuEvents.AudioButtonClicked();
-        public void OnQuestionButton() => MenuEvents.QuestionButtonClicked();
-        public void OnCloseFAQButton() => MenuEvents.CloseFAQPanelClicked();
-        public void OnInfoButton() => MenuEvents.InfoButtonClicked();
-        public void OnExitButton() => MenuEvents.ExitButtonClicked();
-
+    public void OnPlayButton() => MenuEvents.PlayButtonClicked();
+    public void OnControlsButton() => MenuEvents.ControlsButtonClicked();
+    public void OnCloseControlsButton() => MenuEvents.CloseControlsPanelClicked();
+    public void OnMusicButton() => MenuEvents.MusicButtonClicked();
+    public void OnAudioButton() => MenuEvents.AudioButtonClicked();
+    public void OnQuestionButton() => MenuEvents.QuestionButtonClicked();
+    public void OnCloseFAQButton() => MenuEvents.CloseFAQPanelClicked();
+    public void OnInfoButton() => MenuEvents.InfoButtonClicked();
+    public void OnExitButton() => MenuEvents.ExitButtonClicked();
     #endregion
 
     #region Menu Actions
 
-        #region Start Game Functions
-            private void StartGame()
-            {
-                SceneManager.LoadScene("MainGame");
-            } 
-        #endregion
+    #region Start Game Functions
+    private void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    #endregion
 
-        #region Control Functions
-            private void ShowControls()
-            {
-                controlsPanel.SetActive(true);
-            }
-            private void CloseControlsPanel()
-            {
-                controlScroll.value = 1f;
-                controlsPanel.SetActive(false);
-            }
-        #endregion
+    #region Control Functions
+    private void ShowControls()
+    {
+        controlsPanel.SetActive(true);
+    }
+    private void CloseControlsPanel()
+    {
+        controlScroll.value = 1f;
+        controlsPanel.SetActive(false);
+    }
+    #endregion
 
-        #region Voice Functions
-            private void ToggleMusic()
-            {
-                if (musicButton.image.color == new Color(1, 1, 1, 1))
-                {
-                    menuMusic.volume = 0;
-                    musicButton.image.color = new Color(0, 0, 0, 1);
-                }
-                else
-                {
-                    menuMusic.volume = 1;
-                    musicButton.image.color = new Color(1, 1, 1, 1);
-                }
-            }
-            private void OpenAudioSettings()
-            {
-                if (audioSlider.gameObject.activeSelf)
-                {
-                    audioSlider.gameObject.SetActive(false);
-                }
-                else
-                {
-                    audioSlider.gameObject.SetActive(true);
-                }
-            }
+    #region Voice Functions
+    private void ToggleMusic()
+    {
+        if (musicButton.image.color == new Color(1, 1, 1, 1))
+        {
+            menuMusic.volume = 0;
+            musicButton.image.color = new Color(0, 0, 0, 1);
+        }
+        else
+        {
+            menuMusic.volume = 1;
+            musicButton.image.color = new Color(1, 1, 1, 1);
+        }
+    }
+    private void OpenAudioSettings()
+    {
+        if (audioSlider.gameObject.activeSelf)
+        {
+            audioSlider.gameObject.SetActive(false);
+        }
+        else
+        {
+            audioSlider.gameObject.SetActive(true);
+        }
+    }
 
     #endregion
 
-        #region FAQ Functions
-            private void ShowFAQ()
-            {
-                switch(FAQpanel.activeSelf)
-                {
-                    case true:
-                        faqScroll.value = 1f;
-                        FAQpanel.SetActive(false);
-                        break;
-
-                    case false:
-                        FAQpanel.SetActive(true);
-                        break;
-                }           
-            }
-            private void CloseFAQ()
-            {
+    #region FAQ Functions
+    private void ShowFAQ()
+    {
+        switch (FAQpanel.activeSelf)
+        {
+            case true:
                 faqScroll.value = 1f;
                 FAQpanel.SetActive(false);
-            }
+                break;
 
-        #endregion
+            case false:
+                FAQpanel.SetActive(true);
+                break;
+        }
+    }
+    private void CloseFAQ()
+    {
+        faqScroll.value = 1f;
+        FAQpanel.SetActive(false);
+    }
 
-        #region Info Functions
-            private void ShowInfo()
-            {
-                if (infoPanel.activeSelf)
-                {
-                    infoPanel.SetActive(false);
-                }
-                else
-                {
-                    infoPanel.SetActive(true);
-                    currentTime = 0f;
-                }
-            }
     #endregion
 
-        #region Exit Functions
-            private void ExitGame()
-            {
-                #if UNITY_EDITOR
-                    UnityEditor.EditorApplication.isPlaying = false;
-                #else
-                    Application.Quit(); 
-                #endif
-            }
+    #region Info Functions
+    private void ShowInfo()
+    {
+        if (infoPanel.activeSelf)
+        {
+            infoPanel.SetActive(false);
+        }
+        else
+        {
+            infoPanel.SetActive(true);
+            currentTime = 0f;
+        }
+    }
+    #endregion
 
-        #endregion
+    #region Exit Functions
+    private void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                    Application.Quit(); 
+#endif
+    }
+
+    #endregion
 
     #endregion
 
@@ -275,29 +203,28 @@ public class MenuInterface : MonoBehaviour
 
     #region Audio Methods
     private void UpdateAudioSituation(float value)
-            {
-                value *= 0.01f;
-                AudioListener.volume = value;
+    {
+        value *= 0.01f;
+        AudioListener.volume = value;
 
-                if (value == 0)
-                {
-                    audioButton.GetComponent<Image>().color = new Color(0, 0, 0, 1);
-                }
-                else
-                {
-                    audioButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-                }
-            }
-        #endregion
+        if (value == 0)
+        {
+            audioButton.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+        }
+        else
+        {
+            audioButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        }
+    }
+    #endregion
 
-        #region Info Methods
-            private bool CheckInfoPanelTime()
-            {
-                currentTime += Time.deltaTime;
-                return currentTime >= timeToAutoClose;
-            }
-        #endregion
+    #region Info Methods
+    private bool CheckInfoPanelTime()
+    {
+        currentTime += Time.deltaTime;
+        return currentTime >= timeToAutoClose;
+    }
+    #endregion
 
     #endregion
->>>>>>> Staging
 }
